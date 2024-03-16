36b80e72e59ce245e0b28f9fbfc511db97b2a75c
kirtikumar8536 <kirtikumar8536@gmail.com>
2024-03-16 20:32:55 +0530

    first commit

---
+++ src/main/java/com/bigbasket/BigbasketApplication.java
@@ -0,0 +1,13 @@
+package com.bigbasket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigbasketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigbasketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bigbasket/BigbasketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigbasket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigbasketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
